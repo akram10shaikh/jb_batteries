27ff9ed2d5bb07556d19d6035c59cd640e6b0fc4
Akram Shaikh <138965478+akram10shaikh@users.noreply.github.com>
2025-12-27 14:55:09 +0530

    ABC

--- jb_batteries/settings.py
+++ jb_batteries/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%z&$-8cdg0p+h36zw$j9#u&tn5u1u!-if(yi*-bb*ut9_(u7zu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
